de9cd9914ca6a20fb756680c02adf17efcf81a48
SMMC-Digital-Technology <alan.burt@mackillop.act.edu.au>
2017-10-19 12:47:29 +1100

    Added JS files
    Added both the Phaser framework and a blank game file

---
+++ js/game.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+
+  //Create a new game, with dimensions of 800 by 600
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update});
+
+  //Preload function, where we can load all of the assets that will be used
+  function preload() {
+
+  }
+
+  //Create function, where everything is created
+  function create() {
+
+  }
+
+  //This function runs each and every frame
+  function update() {
+
+  }
+};
