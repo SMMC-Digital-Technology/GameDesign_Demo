9f7c295e40813b6a9fb08a191d0a490febad45ad
MrOffner <mroffner@gmail.com>
2017-10-26 15:33:21 +1100

    fix error and add player
    Fixed a typo in the preload (missing apostrophe) & added the player sprite

--- js/game.js
+++ js/game.js
@@ -3,15 +3,17 @@ window.onload = function() {
   //Create a new game, with dimensions of 800 by 600
   var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 
+	var player;
+		
   //Preload function, where we can load all of the assets that will be used
   function preload() {
 		game.load.image('player','assets/player.png');
-		game.load.image('background,'assets/background.png');
+		game.load.image('background','assets/background.png');
   }
 
   //Create function, where everything is created
   function create() {
-
+		player = game.add.sprite(game.world.width/2,game.world.width/2,'player');
   }
 
   //This function runs each and every frame
