fa997bdc525c1546a334d964852b4a3e95551a6a
MrOffner <mroffner@gmail.com>
2017-10-27 12:50:44 +1100

    Started level & powerup
    Some basic code to collect a powerup
    Create 'environment' group with ground
    More comments
    @mrlumley

--- js/game.js
+++ js/game.js
@@ -6,28 +6,38 @@ window.onload = function() {
 	var player;
   var moveKeys;
   var speed = 5;
+	var powerups;
+	var environment;
   
   //Preload function, where we can load all of the assets that will be used
   function preload() {
 		game.load.image('player','assets/player.png');
 		game.load.image('background','assets/background.png');
     game.load.image('enemy', 'assets/enemy.png');
-	game.load.image('powerup','assets/powerUp.png');
+		game.load.image('powerup','assets/powerUp.png');
+		game.load.image('ground', 'assets/ground.png');
 
-	}
+	}	//END of preload
 
   //Create function, where everything is created
   function create() {
 
+		//World creation
 		game.add.sprite(0, 0, 'background');
-
+		
+		environment = game.add.group();
+		environment.enableBody = true;
+		
+		ground = environment.create(0,game.world.height-48,'ground');
+		ground.width = game.world.width;
+		ground.body.immovable = true;
 	
+	
+		//Player creation
 		player = game.add.sprite(game.world.width/2,game.world.width/2,'player');
 		game.physics.enable(player, Phaser.Physics.ARCADE);
     player.body.gravity.y = 200;
     player.body.collideWorldBounds = true;
-
-		game.add.sprite(50,50,'powerup');
 
 		moveKeys = game.input.keyboard.addKeys(
       {
@@ -36,20 +46,33 @@ window.onload = function() {
         'right': Phaser.KeyCode.D
       }
     );
+		
+		powerups = game.add.group();
+		powerups.enableBody = true;
+		powerups.create(50,200,'powerup');
 
-  }
+  }	//END of create
 
   //This function runs each and every frame
   function update() {
+		//Collision events
+		game.physics.arcade.collide(player, environment);
+    game.physics.arcade.overlap(player, powerups, collectPowerup);
 	  
-	 if (moveKeys.left.isDown) {
+	 if (moveKeys.left.isDown) {	//Player moves left
 		 player.x -= speed;
 	 }
-	 else if (moveKeys.right.isDown) {
+	 else if (moveKeys.right.isDown) {	//Player moves right
 		 player.x += speed;
 	 }
-		if(moveKeys.up.isDown){
+		if(moveKeys.up.isDown && player.body.touching.down){	//Player jumps
 			player.body.velocity.y = -350;
 		}
-  }
+  }	//END of update
+	
+	//Increase the player's speed when they collect a powerup
+	function collectPowerup(player, powerup){
+		speed *= 1.2;
+		powerups.remove(powerup);
+	}	//END of collectPowerup
 };
